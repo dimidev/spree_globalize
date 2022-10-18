2a3858bfb25e35459c04c2d1025851ee8dcbc0df
dimidev <dimidev@olympusone.com>
2022-10-18 15:39:54 +0300

    up

--- app/controllers/spree/admin/states_translations_controller.rb
+++ app/controllers/spree/admin/states_translations_controller.rb
@@ -1,6 +1,6 @@
 module Spree
   module Admin
-    class StateTranslationsController < Spree::Admin::TranslationsController
+    class StatesTranslationsController < Spree::Admin::TranslationsController
       private
       def collection_url
         send "admin_country_states_url", @resource.country
